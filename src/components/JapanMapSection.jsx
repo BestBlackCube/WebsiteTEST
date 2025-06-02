75c9bca952ddb13a99ab4fa01feb0935a64e9f1d
BestBlackCube <1cjswosla@gmail.com>
2025-06-02 13:40:34 +0900

    0602-4

--- src/components/JapanMapSection.jsx
+++ src/components/JapanMapSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import mapImage from '../assets/Japan.png';
+import mapImage from '../assets/japan.png';
 import { useNavigate } from 'react-router-dom';
 
 import './JapanMapSection.css';
